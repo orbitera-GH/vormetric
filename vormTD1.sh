e1a8cc8a26a8d15ba426a381a2f39ec878d283a2
harn123 <harn@wp.pl>
2015-08-15 00:54:55 +0200

    zz

--- vormTD1.sh
+++ vormTD1.sh
@@ -6,6 +6,7 @@ azure account list
 azure account set 6313375e-ba66-4c07-89ef-3a02568fd43e
 azure config mode arm
 echo "zabawa z hostname start..."
+HOSTNAME=`hostname`
 new_hostname=`echo ${HOSTNAME} | tr '[:upper:]' '[:lower:]'`
 echo "nazwa hosta z new_hostname: $new_hostname"
 
@@ -17,6 +18,5 @@ grupa=`echo ${HOSTNAME} | cut -d - -f2`
 
 #azure vm create -g $grupa -n VormmetricMain -l westeurope -y linux --vnet-name "${HOSTNAME,,}-net" -d "https://vormetric1td.blob.core.windows.net/vhds/vormetric-${HOSTNAME,,}-image-v2.vhd" -o vormetric1td -R vhds -f vormMainNic --vnet-subnet-name "Subnet-2"
 azure vm create -g $grupa -n VormmetricMain -l westeurope -y linux --vnet-name "$new_hostname-net" -d "https://vormetric1td.blob.core.windows.net/vhds/vormetric-${HOSTNAME,,}-image-v2.vhd" -o vormetric1td -R vhds -f vormMainNic --vnet-subnet-name "Subnet-2"
-
 
 echo "stop." >> /custom.log
