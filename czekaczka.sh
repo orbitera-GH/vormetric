0f203d4a063373f2ae88f7b46a620f53b5c5579b
harn123 <harn@wp.pl>
2015-08-15 02:39:24 +0200

    swsww

--- czekaczka.sh
+++ czekaczka.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -l
+echo "czekaczka start" >> /custom.log
 while ! curl -k https://10.0.76.4/ >/dev/null 2>/dev/null; do
    echo "Waiting..."
    sleep 10
 done
-echo "stop." >> /custom.log
+echo "czekaczka stop." >> /custom.log
