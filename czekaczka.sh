d926f3e40c39d43f6d24a94aa9da0a1d234d31bd
harn123 <harn@wp.pl>
2015-08-15 10:05:34 +0200

    sleep 60

--- czekaczka.sh
+++ czekaczka.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -l
 echo "czekaczka start" >> /custom.log
 while ! curl -k https://10.0.76.4/ >/dev/null 2>/dev/null; do
-   echo "Waiting..."
+   echo "Waiting..." >> /custom.log
    sleep 10
 done
+	sleep 60
 echo "czekaczka stop." >> /custom.log
