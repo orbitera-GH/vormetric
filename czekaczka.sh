c59b2a3a8154f8df2af18363cd334aa16e31e663
harn123 <harn@wp.pl>
2015-09-22 23:20:26 +0200

    poprawka w hasle

--- czekaczka.sh
+++ czekaczka.sh
@@ -5,7 +5,10 @@ new_hostname=`echo ${HOSTNAME} | tr '[:upper:]' '[:lower:]'`
 ID=`echo ${new_hostname} | cut -d - -f2`
 haslo="MySec$ID"
 echo "takie tam.. has: $haslo" >> /tmp/custom.log
-echo -e "$haslo\n$haslo" | sudo passwd root
+#echo -e "$haslo\n$haslo" | sudo passwd root
+echo root:$haslo | sudo chpasswd
+sudo sed -i 's/^PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo restart ssh
 
 while ! curl -k https://10.0.76.4/ >/dev/null 2>/dev/null; do
    echo "Waiting..." >> /tmp/custom.log
