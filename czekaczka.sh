3611b592773424bde3ff77ee7841c066f865f588
harn123 <harn@wp.pl>
2015-09-22 22:05:45 +0200

    dodane haslo roota

--- czekaczka.sh
+++ czekaczka.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 echo "czekaczka start" >> /tmp/custom.log
+HOSTNAME=`hostname`
+new_hostname=`echo ${HOSTNAME} | tr '[:upper:]' '[:lower:]'`
+ID=`echo ${new_hostname} | cut -d - -f2`
+haslo="MySec$ID"
+echo "takie tam.. has: $haslo" >> /tmp/custom.log
+echo -e "$haslo\n$haslo" | sudo passwd root
+
 while ! curl -k https://10.0.76.4/ >/dev/null 2>/dev/null; do
    echo "Waiting..." >> /tmp/custom.log
    sleep 20
