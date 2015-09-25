8d10c443706838586720e1c05bca9a3f574cead9
harn123 <harn@wp.pl>
2015-09-25 14:37:33 +0200

    eastus

--- czekaczka.sh
+++ czekaczka.sh
@@ -1,14 +1,41 @@
 #!/bin/bash
+
 echo "czekaczka start" >> /tmp/custom.log
+
+# we *need* expect
+#sudo apt-get install -y expect
+
+# also we need to patch sshd config
+sudo sed -i 's/^PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sudo restart ssh
+
+# prepare variables
+
 HOSTNAME=`hostname`
 new_hostname=`echo ${HOSTNAME} | tr '[:upper:]' '[:lower:]'`
 ID=`echo ${new_hostname} | cut -d - -f2`
-haslo="MySec$ID"
-echo "takie tam.. has: $haslo" >> /tmp/custom.log
-#echo -e "$haslo\n$haslo" | sudo passwd root
-echo root:$haslo | sudo chpasswd
-sudo sed -i 's/^PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-sudo restart ssh
+
+root_passwd="MySec${ID}"
+goodguy_passwd="${ID}gdg"
+badguy_passwd="bdd${ID}"
+cliadmin_passwd="DsMp@s${ID:2:3}"
+webadmin_passwd="WcAp#s${ID:2:3}"
+
+echo "takie tam: root pass: ${root_passwd}" >> /tmp/custom.log
+echo "takie tam: good-guy pass: ${goodguy_passwd}" >> /tmp/custom.log
+echo "takie tam: bad-guy pass: ${badguy_passwd}" >> /tmp/custom.log
+echo "takie tam: cliadmin DSM pass: ${cliadmin_passwd}" >> /tmp/custom.log
+#echo "takie tam: webadmin DSM pass: ${webadmin_passwd}" >> /tmp/custom.log
+
+# change passwords
+
+sudo chpasswd <<EOF
+root:${root_passwd}
+good-guy:${goodguy_passwd}
+bad-guy:${badguy_passwd}
+EOF
+
+# wait for DSM...
 
 while ! curl -k https://10.0.76.4/ >/dev/null 2>/dev/null; do
    echo "Waiting..." >> /tmp/custom.log
@@ -20,4 +47,21 @@ while [ "`curl -s -o /dev/null -I -w "%{http_code}" -k https://10.0.76.4/`" != "
    echo "Waiting..." >> /tmp/custom.log
    sleep 20
 done
+
+#... and change cliadmin pass
+expect - ${cliadmin_passwd} <<EOF
+set timeout 60
+
+spawn ssh cliadmin@10.0.76.4
+
+expect "yes/no" {
+    send "yes\r"
+    expect "*?assword" { send "cliadmin123\r" }
+    } "*?assword" { send "cliadmin123\r" }
+
+expect "Enter new password  : " { send "[lindex \$argv 0]\r" }
+expect "Enter password again: " { send "[lindex \$argv 0]\r" }
+expect "$ " { send "exit\r" }
+EOF
+
 echo "czekaczka stop." >> /tmp/custom.log
